ecfe5c47f3a15939a254d8648ba17ce0779e58ff
Ananth Ganesan <mail2ananthece@gmail.com>
2024-04-16 11:54:18 +0530

    Category carousel implementation

--- src/Components/ProductCarousel/ProductCarousel.js
+++ src/Components/ProductCarousel/ProductCarousel.js
@@ -4,7 +4,12 @@ import './ProductCarousel.css'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CategoryBanner from '../CategoryBanner/CategoryBanner';
 
+import { Link } from 'react-router-dom'
+
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
 const ProductsCarosel = ({ BgImg, Title, Data }) => {
   const settings = {
     dots: false,
@@ -15,9 +20,45 @@ const ProductsCarosel = ({ BgImg, Title, Data }) => {
     slidesToScroll: 1,
     autoplaySpeed: 3000,
   };
-  return (
-    <div className='product-carousel'>
 
+
+const Next = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <MdArrowForwardIos
+        style={{ color: "black", fontSize: 25, fontWeight: 900 }}
+      />
+    </div>
+  );
+};
+const Prev = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <MdOutlineArrowBackIosNew
+        style={{ color: "black", fontSize: 25, fontWeight: 900 }}
+      />
+    </div>
+  );
+};
+  return (
+    <div className='CategoryCarousel'>
+      <div className='CategoryCarousel-left' style={{ background: `url(${BgImg}) no-repeat 0px bottom` }}>
+        <p>{Title}</p>
+        <button>View all</button>
+      </div>
+      <div className='CategoryCarousel-right'>
+        <Slider {...settings} nextArrow={<Next />} prevArrow={<Prev />}>
+          {Data.map((item, index) => {
+            return (
+              <Link to={"/products"} key={index}>
+                <CategoryBanner ImgSrc={item.ImgSrc} Title={item.CategoryName} Brand={item.Brand} />
+              </Link>
+            )
+          })}
+        </Slider>
+      </div>
     </div>
   )
 }
